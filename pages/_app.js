bf20bc5639c6363b9064d5e73dd617b4f1426a44
rash2x <rash2x@gmail.com>
2021-09-03 17:22:43 +0300

    init styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  </>;
 }
 
-export default MyApp
+export default MyApp;
