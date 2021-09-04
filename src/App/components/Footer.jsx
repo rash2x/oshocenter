ff4001ba8e7d95a116dcd5f34efee58b3b2714e6
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-04 14:44:06 +0300

    Footer component

---
+++ src/App/components/Footer.jsx
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+import Image from 'next/image';
+import Vk from '../../../public/vkIcon.svg';
+import Inst from '../../../public/InstIcon.svg';
+import Telegram from '../../../public/TelegamIcon.svg';
+
+const FooterWrapper = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  padding: 22px 32px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+`;
+
+const Copyright = styled.div`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const Info = styled.div`
+  max-width: 690px;
+`;
+const About = styled.p`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 9px;
+`;
+
+const Contacts = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  text-align: end;
+`;
+const Socials = styled.div`
+  display: flex;
+  margin-bottom: 16px;
+`;
+
+const Icon = styled(Image)``;
+const IconWrapper = styled.div`
+  margin-right: 24px;
+  cursor: pointer;
+
+  &:last-child {
+    margin: 0;
+  }
+`;
+
+const Address = styled.div`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Footer = () => (
+  <FooterWrapper>
+    <Info>
+      <Copyright>&copy; 2021 Центр Ошо Медитаций</Copyright>
+      <About>
+        ОШО является зарегистрированной торговой маркой OSHO International Foundation, используемой
+        с разрешения Некоторые материалы, используемые здесь (изображения и выдержки из текста),
+        защищены авторским правом OSHO International Foundation
+      </About>
+    </Info>
+    <Contacts>
+      <Socials>
+        <IconWrapper>
+          <Link href="/">
+            <Icon src={Vk} />
+          </Link>
+        </IconWrapper>
+        <IconWrapper>
+          <Link href="/">
+            <Icon src={Inst} />
+          </Link>
+        </IconWrapper>
+        <IconWrapper>
+          <Link href="/">
+            <Icon src={Telegram} />
+          </Link>
+        </IconWrapper>
+      </Socials>
+      <Address>
+        Спб, Большая Конюшенная пл. 2В <span>Невский проспект</span>
+      </Address>
+    </Contacts>
+  </FooterWrapper>
+);
+
+export default Footer;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,19 @@
+import styled from 'styled-components';
+import Footer from '../App/components/Footer';
 import Header from '../App/components/Header';
 
+const Base = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+`;
+
 const Home = () => (
-  <div>
+  <Base>
     <Header />
-  </div>
+    <Footer />
+  </Base>
 );
 
 export default Home;
