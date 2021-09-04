eee6e3c200029aab56b416c6b5a4b346655a024d
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-04 10:00:27 +0300

    next files set up

---
+++ pages/_app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+
+// import GlobalStyle from './GlobalStyle';
+import theme from './theme';
+import GlobalStyle from './GlobalStyle';
+
+const MyApp = ({ Component, pageProps }) => (
+  <React.StrictMode>
+    <StyledThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {/* <CssBaseline /> */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledThemeProvider>
+  </React.StrictMode>
+);
+
+export default MyApp;
