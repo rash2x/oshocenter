256210f1f07424012e0e8aaecc70997346b9b5cf
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-04 17:07:21 +0300

    Subscription title fix

--- src/pages/subscription.jsx
+++ src/pages/subscription.jsx
@@ -1,4 +1,3 @@
-import { Container } from '@material-ui/core';
 import styled from 'styled-components';
 import Layout from '../App/components/Layout';
 import SubscriptionCard from '../App/components/SubscriptionCard';
@@ -13,27 +12,27 @@ const Base = styled.div`
 const Title = styled.h1`
   display: flex;
   align-items: baseline;
-  margin: 48px auto;
+  margin: 48px 0;
   font-family: Montserrat;
   font-style: italic;
   font-weight: bold;
   font-size: 32px;
+  display: block;
+  text-align: center;
 `;
 
 const Subscription = () => (
   <Layout>
     <Title>Стоимость и абонементы</Title>
-    <Container maxWidth="md">
-      <Base>
-        <SubscriptionCard />
-        <SubscriptionCard />
-        <SubscriptionCard />
-        <SubscriptionCard />
-        <SubscriptionCard />
-        <SubscriptionCard />
-        <SubscriptionCard />
-      </Base>
-    </Container>
+    <Base>
+      <SubscriptionCard />
+      <SubscriptionCard />
+      <SubscriptionCard />
+      <SubscriptionCard />
+      <SubscriptionCard />
+      <SubscriptionCard />
+      <SubscriptionCard />
+    </Base>
   </Layout>
 );
 
